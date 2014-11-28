f3a93fc3adfff023470868db4f8cdab149595c99
gilgil <admin@gilgil.net>
2014-11-28 17:37:09 +0900

    Add VDebugNewCancel

---
+++ include/common/vdebugnewcancel.h
@@ -0,0 +1,22 @@
+// ----------------------------------------------------------------------------
+//
+// VDream Component Suite version 9.1
+//
+// http://www.gilgil.net
+//
+// Copyright (c) Gilbert Lee All rights reserved
+//
+// ----------------------------------------------------------------------------
+
+#ifndef __V_DEBUG_NEW_CANCEL_H__
+#define __V_DEBUG_NEW_CANCEL_H__
+
+#ifdef _DEBUG
+
+#undef debug_new
+#define debug_new new
+
+#endif // _DEBUG
+
+#endif // __V_DEBUG_NEW_CANCEL_H__
+

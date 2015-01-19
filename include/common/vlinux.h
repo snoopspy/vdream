139f33bd29d92291a839e8660ad16c7f851b7417
gilgil <admin@gilgil.net>
2015-01-19 11:14:50 +0900

    Change integer type

--- include/common/vlinux.h
+++ include/common/vlinux.h
@@ -40,18 +40,18 @@ typedef int  BOOL;
 typedef void (*HMODULE);
 
 typedef signed char        INT8,   *PINT8;
-typedef signed short       INT16,  *PINT16;
-typedef signed int         INT32,  *PINT32;
+typedef signed short       int16_t,  *PINT16;
+typedef signed int         int32_t,  *PINT32;
 typedef signed long long   INT64,  *PINT64;
 
 typedef unsigned char      uint8_t,  *PUINT8;
-typedef unsigned short     UINT16, *PUINT16;
-typedef unsigned int       UINT32, *PUINT32;
+typedef unsigned short     uint16_t, *PUINT16;
+typedef unsigned int       uint32_t, *PUINT32;
 typedef unsigned long long UINT64, *PUINT64;
 
 typedef unsigned long      ULONG_PTR, *PULONG_PTR;
 typedef ULONG_PTR          DWORD_PTR, *PDWORD_PTR;
-typedef unsigned char      BYTE,   *PBYTE;
+typedef unsigned char      uint8_t,   *PBYTE;
 typedef unsigned short     WORD,   *PWORD;
 typedef unsigned int       DWORD,  *PDWORD;
 */
@@ -62,12 +62,12 @@ typedef unsigned int       DWORD,  *PDWORD;
 // ----------------------------------------------------------------------------
 // Macro
 // ----------------------------------------------------------------------------
-#define MAKEWORD(a, b)      ((WORD)(((BYTE)(((DWORD_PTR)(a)) & 0xff)) | ((WORD)((BYTE)(((DWORD_PTR)(b)) & 0xff))) << 8))
+#define MAKEWORD(a, b)      ((WORD)(((uint8_t)(((DWORD_PTR)(a)) & 0xff)) | ((WORD)((uint8_t)(((DWORD_PTR)(b)) & 0xff))) << 8))
 #define MAKELONG(a, b)      ((LONG)(((WORD)(((DWORD_PTR)(a)) & 0xffff)) | ((DWORD)((WORD)(((DWORD_PTR)(b)) & 0xffff))) << 16))
 #define LOWORD(l)           ((WORD)(((DWORD_PTR)(l)) & 0xffff))
 #define HIWORD(l)           ((WORD)((((DWORD_PTR)(l)) >> 16) & 0xffff))
-#define LOBYTE(w)           ((BYTE)(((DWORD_PTR)(w)) & 0xff))
-#define HIBYTE(w)           ((BYTE)((((DWORD_PTR)(w)) >> 8) & 0xff))
+#define LOBYTE(w)           ((uint8_t)(((DWORD_PTR)(w)) & 0xff))
+#define HIBYTE(w)           ((uint8_t)((((DWORD_PTR)(w)) >> 8) & 0xff))
 */
 // ----------------------------------
 
